b6143343c8042dcab7db4b6280756fde0b525823
Mauricio Erazo <antonierazo13@hotmail.com>
2022-11-25 11:07:42 +0100

    Feat: creating event listener to add a note when we click the button

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ function addNewNote(text = '') {
   const note = document.createElement('div')
   note.classList.add('note')
 
+  // "div class=main" if text then have no class, else then have a class of hidden.
+  // "textarea" if text has a class of hidden, else have no class
   note.innerHTML = `
     <div class="tools">
       <button class="edit"><i class="fas fa-edit"></i></button>
       <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
-
-    <div class="main">
-      <textarea></textarea>
-    </div>
+    
+    <div class="main ${text ? "" : "hidden"} "></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
     `
+
+    // we add the note every time we click
+    document.body.appendChild(note)
 } 
